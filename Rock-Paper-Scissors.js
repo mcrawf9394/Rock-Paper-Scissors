45b0d69895e14001a96f6c728548d6f8457ea9d8
Michael Crawford <mjc9394@gmail.com>
2024-02-21 15:42:19 -0500

    Still having issues with the prompt not filling into the function properly, attempted a loop but ultimately tried to just copy the functions to see if it could fix the prompt not filling in, it did not

--- Rock-Paper-Scissors.js
+++ Rock-Paper-Scissors.js
@@ -1,11 +1,9 @@
-
-function playGame (){
-    let i = 0
-    let U = 0
-    let C = 0
-    do {
-        let playerSelection = prompt();
-        let choice = getComputerChoice()
+let i = 0
+let U = 0
+let C = 0
+    
+let playerSelection = prompt();
+let choice = getComputerChoice()
         function getComputerChoice() {
             const choices= ["rock", "paper", "scissors"];
             const num = Math.floor(Math.random()*3);
@@ -31,9 +29,126 @@ function playGame (){
         else 
             message = "Something went wrong!"
         return message;
-         } console.log(playRound(playerSelection, choice))
-} while (i<5)
+         } 
+         console.log(playRound(playerSelection, choice))
+         playerSelection = prompt();
+         choice = getComputerChoice()
+        function getComputerChoice() {
+            const choices= ["rock", "paper", "scissors"];
+            const num = Math.floor(Math.random()*3);
+            const computerChoice = choices[num]
+            return computerChoice;
+        }
+        function playRound (playerSelection, choice) {
+        let message
+        if (playerSelection == choice)
+            message = "Draw!"
+        else if (playerSelection == "rock" && choice == "paper",i++,C++)
+            message = "You Lose! Paper beats rock!"
+        else if (playerSelection == "rock" && choice == "scissors",U++,i++)
+            message = "You Win! Rock beats scissors!"
+        else if (playerSelection == "paper" && choice == "rock", U++,i++)
+            message = "You Win! Paper beats rock!"
+        else if (playerSelection == "paper" && choice == "scissors", C++,i++)
+            message = "You Lose! Scissors beats paper!"
+        else if (playerSelection == "scissors" && choice == "paper", U++,i++)
+            message = "You Win! Scissors beats paper!"
+        else if (playerSelection == "scissors" && choice == "rock",C++,i++)
+            message = "You Lose! Rock beats scissors!"
+        else 
+            message = "Something went wrong!"
+        return message;
+         } 
+         console.log(playRound(playerSelection, choice))
+         playerSelection = prompt();
+         choice = getComputerChoice()
+        function getComputerChoice() {
+            const choices= ["rock", "paper", "scissors"];
+            const num = Math.floor(Math.random()*3);
+            const computerChoice = choices[num]
+            return computerChoice;
+        }
+        function playRound (playerSelection, choice) {
+        let message
+        if (playerSelection == choice)
+            message = "Draw!"
+        else if (playerSelection == "rock" && choice == "paper",i++,C++)
+            message = "You Lose! Paper beats rock!"
+        else if (playerSelection == "rock" && choice == "scissors",U++,i++)
+            message = "You Win! Rock beats scissors!"
+        else if (playerSelection == "paper" && choice == "rock", U++,i++)
+            message = "You Win! Paper beats rock!"
+        else if (playerSelection == "paper" && choice == "scissors", C++,i++)
+            message = "You Lose! Scissors beats paper!"
+        else if (playerSelection == "scissors" && choice == "paper", U++,i++)
+            message = "You Win! Scissors beats paper!"
+        else if (playerSelection == "scissors" && choice == "rock",C++,i++)
+            message = "You Lose! Rock beats scissors!"
+        else 
+            message = "Something went wrong!"
+        return message;
+         } 
+         console.log(playRound(playerSelection, choice))
+         playerSelection = prompt();
+         choice = getComputerChoice()
+        function getComputerChoice() {
+            const choices= ["rock", "paper", "scissors"];
+            const num = Math.floor(Math.random()*3);
+            const computerChoice = choices[num]
+            return computerChoice;
+        }
+        function playRound (playerSelection, choice) {
+        let message
+        if (playerSelection == choice)
+            message = "Draw!"
+        else if (playerSelection == "rock" && choice == "paper",i++,C++)
+            message = "You Lose! Paper beats rock!"
+        else if (playerSelection == "rock" && choice == "scissors",U++,i++)
+            message = "You Win! Rock beats scissors!"
+        else if (playerSelection == "paper" && choice == "rock", U++,i++)
+            message = "You Win! Paper beats rock!"
+        else if (playerSelection == "paper" && choice == "scissors", C++,i++)
+            message = "You Lose! Scissors beats paper!"
+        else if (playerSelection == "scissors" && choice == "paper", U++,i++)
+            message = "You Win! Scissors beats paper!"
+        else if (playerSelection == "scissors" && choice == "rock",C++,i++)
+            message = "You Lose! Rock beats scissors!"
+        else 
+            message = "Something went wrong!"
+        return message;
+         } 
+         console.log(playRound(playerSelection, choice))
+         playerSelection = prompt();
+         choice = getComputerChoice()
+        function getComputerChoice() {
+            const choices= ["rock", "paper", "scissors"];
+            const num = Math.floor(Math.random()*3);
+            const computerChoice = choices[num]
+            return computerChoice;
+        }
+        function playRound (playerSelection, choice) {
+        let message
+        if (playerSelection == choice)
+            message = "Draw!"
+        else if (playerSelection == "rock" && choice == "paper",i++,C++)
+            message = "You Lose! Paper beats rock!"
+        else if (playerSelection == "rock" && choice == "scissors",U++,i++)
+            message = "You Win! Rock beats scissors!"
+        else if (playerSelection == "paper" && choice == "rock", U++,i++)
+            message = "You Win! Paper beats rock!"
+        else if (playerSelection == "paper" && choice == "scissors", C++,i++)
+            message = "You Lose! Scissors beats paper!"
+        else if (playerSelection == "scissors" && choice == "paper", U++,i++)
+            message = "You Win! Scissors beats paper!"
+        else if (playerSelection == "scissors" && choice == "rock",C++,i++)
+            message = "You Lose! Rock beats scissors!"
+        else 
+            message = "Something went wrong!"
+        return message;
+         } 
+         console.log(playRound(playerSelection, choice))
     let finalMessage
+    function finalPlay (){
     if (U = C)
         finalMessage = "It's a tie!"
     else if (U > C)
@@ -41,5 +156,4 @@ function playGame (){
     else
         finalMessage = "You Lose!"
     return finalMessage;
-}
-console.log(playGame())
+    }
