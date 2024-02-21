0dd7fe45e8dc3b3b74220912e9a097de8edae8d1
Michael Crawford <mjc9394@gmail.com>
2024-02-21 14:24:54 -0500

    1st Attempt at making a loop of the program, the prompt stays the same as the orginal value even though it is inside the loop

--- Rock-Paper-Scissors.js
+++ Rock-Paper-Scissors.js
@@ -1,31 +1,45 @@
 
-function getComputerChoice() {
-    const choices= ["rock", "paper", "scissors"];
-    const num = Math.floor(Math.random()*3);
-    const computerChoice = choices[num]
-    return computerChoice;
+function playGame (){
+    let i = 0
+    let U = 0
+    let C = 0
+    do {
+        let playerSelection = prompt();
+        let choice = getComputerChoice()
+        function getComputerChoice() {
+            const choices= ["rock", "paper", "scissors"];
+            const num = Math.floor(Math.random()*3);
+            const computerChoice = choices[num]
+            return computerChoice;
+        }
+        function playRound (playerSelection, choice) {
+        let message
+        if (playerSelection == choice)
+            message = "Draw!"
+        else if (playerSelection == "rock" && choice == "paper",i++,C++)
+            message = "You Lose! Paper beats rock!"
+        else if (playerSelection == "rock" && choice == "scissors",U++,i++)
+            message = "You Win! Rock beats scissors!"
+        else if (playerSelection == "paper" && choice == "rock", U++,i++)
+            message = "You Win! Paper beats rock!"
+        else if (playerSelection == "paper" && choice == "scissors", C++,i++)
+            message = "You Lose! Scissors beats paper!"
+        else if (playerSelection == "scissors" && choice == "paper", U++,i++)
+            message = "You Win! Scissors beats paper!"
+        else if (playerSelection == "scissors" && choice == "rock",C++,i++)
+            message = "You Lose! Rock beats scissors!"
+        else 
+            message = "Something went wrong!"
+        return message;
+         } console.log(playRound(playerSelection, choice))
+} while (i<5)
+    let finalMessage
+    if (U = C)
+        finalMessage = "It's a tie!"
+    else if (U > C)
+        finalMessage = "You Win!"
+    else
+        finalMessage = "You Lose!"
+    return finalMessage;
 }
-let choice = getComputerChoice()
-let playerSelection = prompt();
-playerSelection.toLowerCase
-function playRound (playerSelection, choice) {
-    let message
-    if (playerSelection == choice)
-        message = "Draw!"
-    else if (playerSelection == "rock" && choice == "paper")
-        message = "You Lose! Paper beats rock!"
-    else if (playerSelection == "rock" && choice == "scissors")
-        message = "You Win! Rock beats scissors!"
-    else if (playerSelection == "paper" && choice == "rock")
-        message = "You Win! Paper beats rock!"
-    else if (playerSelection == "paper" && choice == "scissors")
-        message = "You Lose! Scissors beats paper!"
-    else if (playerSelection == "scissors" && choice == "paper")
-        message = "You Win! Scissors beats paper!"
-    else if (playerSelection == "scissors" && choice == "rock")
-        message = "You Lose! Rock beats scissors!"
-    else 
-        message = "Something went wrong!"
-    return message;
-}
-console.log(playRound(playerSelection, choice))
+console.log(playGame())
